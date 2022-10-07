56de500f7d325046bfcf96f1b5d9d21a7d2f1da7
Siobhan Drohan <sdrohan@wit.ie>
2022-10-07 13:12:15 +0100

    Test the DB created using pgAdmin SQL Scripts

--- src/main/kotlin/ie/setu/config/DbConfig.kt
+++ src/main/kotlin/ie/setu/config/DbConfig.kt
@@ -11,11 +11,18 @@ class DbConfig{
 
         logger.info{"Starting DB Connection..."}
 
-        val PGUSER = "kuwsemue"
-        val PGPASSWORD = "zWmiFoHcieiOZIPOf-bIxWDnkzYZRl91"
         val PGHOST = "lucky.db.elephantsql.com"
         val PGPORT = "5432"
-        val PGDATABASE = "kuwsemue"
+
+        //Test database created using handcoded SQL and Insert Statements
+        //val PGUSER = "kuwsemue"
+        //val PGPASSWORD = "zWmiFoHcieiOZIPOf-bIxWDnkzYZRl91"
+        //val PGDATABASE = "kuwsemue"
+
+        //Test database created using generated SQL and Insert Statements from Heroku
+        val PGUSER = "nhybnlbf"
+        val PGPASSWORD = "6LIYvcWKzfNIBFP0kaw2t56NJWO6UXKq"
+        val PGDATABASE = "nhybnlbf"
 
         //url format should be jdbc:postgresql://host:port/database
         val url = "jdbc:postgresql://$PGHOST:$PGPORT/$PGDATABASE"
