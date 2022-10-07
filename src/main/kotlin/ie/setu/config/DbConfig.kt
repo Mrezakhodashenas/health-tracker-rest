16f4c88d2805fad98869337b9490cc6247b333ea
Siobhan Drohan <sdrohan@wit.ie>
2022-10-07 12:28:31 +0100

    Change to ElephantSQL and Railway

--- src/main/kotlin/ie/setu/config/DbConfig.kt
+++ src/main/kotlin/ie/setu/config/DbConfig.kt
@@ -1,18 +1,34 @@
 package ie.setu.config
 
 import org.jetbrains.exposed.sql.Database
+import mu.KotlinLogging
 
 class DbConfig{
 
-    //NOTE: you need the ?sslmode=require otherwise you get an error complaining about the ssl certificate
-    fun getDbConnection(): Database {
+    fun getDbConnection() :Database{
 
-        return Database.connect(
-            "jdbc:postgresql://ec2-54-147-33-38.compute-1.amazonaws.com:5432/dd1alnffpluckj?sslmode=require",
-            driver = "org.postgresql.Driver",
-            user = "decoqpkgvcdpue",
-            password = "c03e409c7da188b020d3be9238b88e61fa4b4fb4ecd0d6551523dec6daf1d78a"
+        val logger = KotlinLogging.logger {}
+
+        logger.info{"Starting DB Connection..."}
+
+        val PGUSER = "kuwsemue"
+        val PGPASSWORD = "zWmiFoHcieiOZIPOf-bIxWDnkzYZRl91"
+        val PGHOST = "lucky.db.elephantsql.com"
+        val PGPORT = "5432"
+        val PGDATABASE = "kuwsemue"
+
+        //url format should be jdbc:postgresql://host:port/database
+        val url = "jdbc:postgresql://$PGHOST:$PGPORT/$PGDATABASE"
+
+        val dbConfig = Database.connect(url,
+            driver="org.postgresql.Driver",
+            user = PGUSER,
+            password = PGPASSWORD
         )
+
+        logger.info{"db url - connection: " + dbConfig.url}
+
+        return dbConfig
     }
 
 }
